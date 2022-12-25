de7bb544249b83ad0bac350cb3a23fb4f694be11
rodri1013 <94713178+rodri1013@users.noreply.github.com>
2022-12-25 19:07:57 -0300

    index route ok

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const pokemonRoute = require('./pokemonRoute.js');
+const typeRoute = require('./typeRoute.js');
 
 
 const router = Router();
+router.use('/pokemon', pokemonRoute);// middleware same as router.get
+router.use('/type', typeRoute);
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
